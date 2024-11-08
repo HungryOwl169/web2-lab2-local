c972b93c0a3312c9e77e0188d36dc7356c4cc713
ante <148277841+HungryOwl169@users.noreply.github.com>
2024-11-08 21:10:42 +0100

    code update

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
-  port: process.env.PORT, 
-  database: process.env.DATABASE
+  port: 5432,
+  database: process.env.DATABASE,
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params)
+  query: (text, params) => pool.query(text, params),
 };
