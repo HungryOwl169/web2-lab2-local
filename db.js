df66e8e61cb1719de08fa43029934cd593784ccf
ante <148277841+HungryOwl169@users.noreply.github.com>
2024-11-08 21:32:01 +0100

    code update

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: 'database_21g8_user',
-  password: 'XpXEcGJrARafKCftnEDwkoLZEVywbAG0',
-  host: 'dpg-csf6dr88fa8c739sgegg-a',
-  port: 5432, 
-  database: 'database_21g8'
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: 5432,
+  database: process.env.DATABASE,
 });
 
 module.exports = {
